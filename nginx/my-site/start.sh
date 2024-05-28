9332f8f9404a5df0b201e1bc4caff9bb859422b2
RyanChen1997 <yongxiangchen69@outlook.com>
2024-05-28 22:23:59 +0800

    nginx

--- nginx/my-site/start.sh
+++ nginx/my-site/start.sh
@@ -1,4 +1,10 @@
+# 启动nginx
+# 同时配置https ssl证书
+# 证书地址根据需要改
 docker run -d --network=host --rm  --name my-web\
-    -v ./nginx/my-site/nginx.conf:/etc/nginx/conf.d/default.conf \
+    -v /home/dev/dev/qisimiaoxiang.site_nginx/qisimiaoxiang.site_bundle.crt:/etc/nginx/qisimiaoxiang.site_bundle.crt \
+    -v /home/dev/dev/qisimiaoxiang.site_nginx/qisimiaoxiang.site.key:/etc/nginx/qisimiaoxiang.site.key \
+    -v ./nginx/my-site/http.conf:/etc/nginx/conf.d/http.conf \
+    -v ./nginx/my-site/https.conf:/etc/nginx/conf.d/https.conf \
     -v ./css-study/my_site/:/usr/share/nginx/html \
     -v ./nginx/my-site/log:/var/log/nginx nginx
